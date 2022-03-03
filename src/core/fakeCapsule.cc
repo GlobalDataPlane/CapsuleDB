c6920c28c85add0139e08b7c2c1462dcc9f2ed02
Ryan Teoh <ryanteoh01@gmail.com>
2022-03-03 12:23:02 -0800

    Refactoring put & get to user protobuf

--- src/core/fakeCapsule.cc
+++ src/core/fakeCapsule.cc
@@ -1,4 +1,3 @@
-#include <fstream>
 #include <openssl/sha.h>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
@@ -6,6 +5,7 @@
 #include <iostream>
 #include "capsuleBlock.hh"
 #include "fakeCapsule.hh"
+#include "capsuleBlock.pb.h""
 
 void sha256_string(const char *string, char outputBuffer[65])
 {
@@ -31,49 +31,72 @@ void sha256_string(const char *string, char outputBuffer[65])
 */
 std::string putCapsuleBlock(CapsuleBlock inputBlock) {
     // Serialize Block
-    std::stringstream toBeHashed;
-    boost::archive::text_oarchive oa1(toBeHashed);
-    oa1 << inputBlock;
-    std::string s = toBeHashed.str();
-    // std::cout << "putCapsuleBlock: toBeHashed=" << s << "\n";
+    serialized::CapsuleBlock protobufBlock;
+    protobufBlock.set_level(inputBlock.getLevel());
+    protobufBlock.set_startkey(inputBlock.getMinKey());
+    protobufBlock.set_endkey(inputBlock.getMaxKey());
+    protobufBlock.mutable_kvpairs() = {inputBlock.getKVPairs().begin(),
+                                         inputBlock.getKVPairs().end()};
+    std::string serializedBlock;
+    if (!protobufBlock.SerializeToString(serializedBlock.data())) {
+        std::cerr << "Failed to serialize CapsuleBlock" << endl;
+        return "";
+    }
+    // std::cout << "putCapsuleBlock: toBeHashed=" << serializedBlock << "\n";
 
     // Hash bytestream
     char blockHash[65];
-    sha256_string(s.data(), blockHash);
+    sha256_string(serializedBlock.data(), blockHash);
     // std::cout << "putCapsuleBlock: blockHash=" << blockHash << "\n";
 
-    // Serialize and store block
+    // Store serialized block in file
     std::ofstream storedBlockFile(blockHash);
-    boost::archive::text_oarchive oa2(storedBlockFile);
-    oa2 << inputBlock;
+    storedBlockFile << serializedBlock;
+    sotredBlockFile.close();
 
-    // Return Hash
+    // return Hash
     return blockHash;
 }
 
 CapsuleBlock getCapsuleBlock(std::string inputHash) {
-    CapsuleBlock recoveredBlock;
+    serialized::CapsuleBlock recoveredBlock;
     // Retrieve and deserialize block
     std::ifstream storedBlock(inputHash);
-    boost::archive::text_iarchive ia(storedBlock);
-    ia >> recoveredBlock;
+    if (!recoveredBlock.ParseFromIstream(&storedBlock)) {
+        std::cerr << "Failed to parse CapsuleBlock" << endl;
+        return NULL;
+    }
 
     // * Re-serialize and check hash *
     // ** Serialize Block **
-    std::stringstream toBeHashed;
-    boost::archive::text_oarchive oa1(toBeHashed);
-    oa1 << recoveredBlock;
-    std::string s = toBeHashed.str();
-    // std::cout << "getCapsuleBlock: toBeHashed=" << s << "\n";
+    std::string serializedBlock;
+    if (!protobufBlock.SerializeToString(serializedBlock.data())) {
+        std::cerr << "Failed to serialize CapsuleBlock" << endl;
+        return NULL;
+    }
+    // std::cout << "getCapsuleBlock: toBeHashed=" << serializedBlock << "\n";
     // ** Hash bytestream ** 
     char blockHash[65];
-    sha256_string(s.data(), blockHash);
+    sha256_string(serializedBlock.data(), blockHash);
     // std::cout << "getCapsuleBlock: blockHash=" << blockHash << "\n";
     // ** Verify hash **
     if (blockHash != inputHash) {
         std::cout << "inputHash=" << inputHash << "\n";
         throw std::invalid_argument("inputHash not found");
     }
+
+    // Convert recoveredBlock(proto) to actual CapsuleBlock
+    CapsuleBlock actualBlock(serializedBlock.level());
+    actualBlock.setMinKey(serializedBlock.startkey());
+    actualBlock.setMaxKey(serializedBlock.endkey());
+    for (const serialized::CapsuleBlock &kvPair_pb: serializedBlock.kvPairs) {
+
+      struct kvs_payload kvPair = {kvPair_pb.key(), kvPair_pb.value(),
+                                   kvPair_pb.txn_timestamp(),
+                                   kvPair_pb.txn_msgtype()};
+      actualBlock.addKVPair(kvPair);
+    }
+
     // Return to user
-    return recoveredBlock;
+    return actualBlock;
 }
